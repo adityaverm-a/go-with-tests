678e38c3ba5d01a5976bdf44b316488d9f7b53b2
adityaverm-a <aditya.se.403@gmail.com>
2022-07-25 00:52:59 +0530

    integers | refactored

--- integers/addr_test.go
+++ integers/addr_test.go
@@ -1,6 +1,16 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+// Run go test -v
+func ExampleAdd() {
+	sum := Add(6, 5)
+	fmt.Println(sum)
+	// Output: 11
+}
 
 func TestAddr(t *testing.T) {
 	sum := Add(3, 2)
