459363cf6b363459f60f7d2495a973f8b19f849c
adityaverm-a <aditya.se.403@gmail.com>
2022-07-24 23:31:08 +0530

    refactoring | added a constant

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // It is good to separate your "domain" code from the outside world (side-effects).
 //The fmt.Println is a side effect (printing to stdout) and the string we send in is our domain.
 func Hello(name string) string {
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
 
 func main() {
