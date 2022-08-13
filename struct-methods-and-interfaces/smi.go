3d2f5cae85dbbb45ab2975f3fe9c393d8631fb60
adityaverm-a <aditya.se.403@gmail.com>
2022-08-13 17:44:05 +0530

    method | wrote a failing test

--- struct-methods-and-interfaces/smi.go
+++ struct-methods-and-interfaces/smi.go
@@ -1,5 +1,22 @@
 package smi
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 0
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
