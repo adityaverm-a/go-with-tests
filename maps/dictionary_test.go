c9bbcabfa70aeb6c91636df9d254ec1c1e20322e
adityaverm-a <aditya.se.403@gmail.com>
2022-09-14 18:47:09 +0530

    maps | wrote a failing test case

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,24 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "This is just a test!"}
 
-	got := Dictionary.Search(dictionary, "test")
-	want := "This is just a test!"
+	t.Run("Known word", func(t *testing.T) {
+		got, _ := Dictionary.Search(dictionary, "test")
+		want := "This is just a test!"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("Unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		want := "could not findthe word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
