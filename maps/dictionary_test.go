1a73cca80169423d0165ff4d285560039da4325a
adityaverm-a <aditya.se.403@gmail.com>
2022-09-15 10:56:20 +0530

    maps | passed the failing test case

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -20,13 +20,26 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "prince"
-	definition := "Saiyan Prince Vegeta"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "prince"
+		definition := "Saiyan Prince Vegeta"
 
-	dictionary.Add(word, definition)
+		err := dictionary.Add(word, definition)
 
-	assertDefinition(t, dictionary, word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "prince"
+		definition := "Saiyan Prince Vegeta"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
