db63147605a8e56be6ba0f537717cb7992d7f61f
adityaverm-a <aditya.se.403@gmail.com>
2022-10-07 09:10:31 +0530

    mocking | wrote a failing test case

--- mocking/mock.go
+++ mocking/mock.go
@@ -10,7 +10,19 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		time.Sleep(1 * time.Second)
@@ -19,6 +31,13 @@ func Countdown(out io.Writer) {
 	fmt.Fprint(out, finalWord)
 }
 
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
