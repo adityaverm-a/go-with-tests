e5f507a9d3018fc458cd19498bea6f8bc4c6b35f
adityaverm-a <aditya.se.403@gmail.com>
2022-11-03 18:24:05 +0530

    concurrency | wrapping up

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -2,12 +2,39 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsite(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
 	}
 
 	return results
 }
+
+// Quick summary
+
+// - Go Routines, the basic unit of concurrency in Go, which let us
+// check more than one website at the same time.
+
+// - Anonymous Functions, which we used to start each of the
+// concurrent processes that check websites.
+
+// - Channels, to help organize and control the communication
+// between the different processes, allowing us to avoid a race
+// condition bug.
+
+// - The Race Detector which helped us debug problems with concurrent code
